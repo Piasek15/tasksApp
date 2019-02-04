22cba2fcfeeaf16f6c573878bac558c5bb16b48c
Patrick <piasecki.patrick.15@gmail.com>
2019-02-04 21:29:19 +0100

    adding typescript Task class

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task1",true, "15-07-1993"))
+        this.tasks.push(new Task(2, "Task2",false, "15-07-1993"))
+        this.tasks.push(new Task(3, "Task3",false, "15-07-1993"))
+    }
+
+    onTaskChange(event, task) {
+        console.log("Task has changed")
+    }
+
+    getDueDateBadge(task: Task) {
+        return task.completed ? 'badge-success' : 'badge-primary';
+    }
 }
