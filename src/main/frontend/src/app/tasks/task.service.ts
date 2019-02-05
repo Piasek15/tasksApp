cbda60f215e87ea2d41545e38aa1c8e8ef9a9d4c
Patrick <piasecki.patrick.15@gmail.com>
2019-02-05 20:55:12 +0100

    adding saveTask method

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,5 +1,6 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {Task} from "./task.model";
 
 @Injectable()
 export class TaskService {
@@ -10,5 +11,10 @@ export class TaskService {
 
     getTasks(){
         return this.http.get('/api/tasks');
+    }
+
+    saveTask(task: Task, checked: boolean){
+        task.completed = checked;
+        return this.http.post('/api/tasks/save', task);
     }
 }
